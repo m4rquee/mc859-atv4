1f4f1e569b23ac7632dd40534d833c4cba8d1ba0
m4rquee <lucasxm12@gmail.com>
2022-05-11 21:41:04 -0300

    grasp project base

---
+++ GRASP-Framework/src/solutions/Solution.java
@@ -0,0 +1,23 @@
+package solutions;
+
+import java.util.ArrayList;
+
+public class Solution<E> extends ArrayList<E> {
+
+    public Double cost = Double.POSITIVE_INFINITY;
+
+    public Solution() {
+        super();
+    }
+
+    public Solution(Solution<E> sol) {
+        super(sol);
+        cost = sol.cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Solution: cost=[" + cost + "], size=[" + this.size() + "], elements=" + super.toString();
+    }
+}
+
