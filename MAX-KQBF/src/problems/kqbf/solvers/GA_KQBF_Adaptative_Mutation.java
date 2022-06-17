d5703718a0d5dce639c007b85f7a813c437133c7
m4rquee <lucasxm12@gmail.com>
2022-06-17 13:43:28 -0300

    some refac and missing fixChromosome added

--- MAX-KQBF/src/problems/kqbf/solvers/GA_KQBF_Adaptative_Mutation.java
+++ MAX-KQBF/src/problems/kqbf/solvers/GA_KQBF_Adaptative_Mutation.java
@@ -4,24 +4,28 @@ import java.io.IOException;
 
 public class GA_KQBF_Adaptative_Mutation extends GA_KQBF {
 
+    public static final double MULTIPLIER = 3.0; // how much the mutation will increase for below mean individuals
+
     public GA_KQBF_Adaptative_Mutation(Integer popSize, Double mutationRate, String filename) throws IOException {
         super(popSize, mutationRate, filename);
     }
 
     @Override
     protected Population mutate(Population offsprings) {
-        Double sumFitness = 0.0;
-        Double meanFitness;
+        Double sumFitness = 0.0, meanFitness;
         for (Chromosome c : offsprings)
             sumFitness += c.fitness;
         meanFitness = sumFitness / popSize;
 
         for (Chromosome c : offsprings) {
-            var currMutationRate = c.fitness > meanFitness ? mutationRate : mutationRate * 3;
-            currMutationRate = Math.min(currMutationRate, 1.0);
-            for (int locus = 0; locus < chromosomeSize; locus++)
-                if (rng.nextDouble() < currMutationRate)
-                    mutateGene(c, locus);
+            var currMutationRate = c.fitness >= meanFitness ? mutationRate : mutationRate * MULTIPLIER;
+            if (rng.nextDouble() < currMutationRate) {
+                for (int locus = 0; locus < chromosomeSize; locus++)
+                    if (rng.nextDouble() < currMutationRate / 10)
+                        mutateGene(c, locus);
+                fixChromosome(c); // make the invalid chromosomes viable
+                c.fitness = fitness(c);
+            }
         }
 
         return offsprings;
