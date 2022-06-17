8f5d3b5b5d7554cddee2dad0bc2e21f689312251
m4rquee <lucasxm12@gmail.com>
2022-06-17 13:44:29 -0300

    performance improvements and missing fixChromosome added

--- MAX-KQBF/src/problems/kqbf/solvers/GA_KQBF_Uniform_Crossover.java
+++ MAX-KQBF/src/problems/kqbf/solvers/GA_KQBF_Uniform_Crossover.java
@@ -4,36 +4,44 @@ import java.io.IOException;
 
 public class GA_KQBF_Uniform_Crossover extends GA_KQBF {
 
-	public GA_KQBF_Uniform_Crossover(Integer popSize, Double mutationRate, String filename) throws IOException {
-		super(popSize, mutationRate, filename);
-	}
+    public GA_KQBF_Uniform_Crossover(Integer popSize, Double mutationRate, String filename) throws IOException {
+        super(popSize, mutationRate, filename);
+    }
 
-	@Override
-	protected Population crossover(Population parents) {
-		Population offsprings = new Population();
+    @Override
+    protected Population crossover(Population parents) {
+        Population offsprings = new Population();
 
-		for (int i = 0; i < popSize; i = i + 2) {
-			Chromosome parent1 = parents.get(i), parent2 = parents.get(i + 1);
-			Chromosome offspring1 = new Chromosome(), offspring2 = new Chromosome();
+        for (int i = 0; i < popSize; i = i + 2) {
+            Chromosome parent1 = parents.get(i), parent2 = parents.get(i + 1);
 
-			for (int j = 0; j < chromosomeSize; j++)
-				if (Math.random() < 0.5) {
-					offspring1.add(parent1.get(j));
-					offspring2.add(parent2.get(j));
-				} else {
-					offspring1.add(parent2.get(j));
-					offspring2.add(parent1.get(j));
-				}
+            if (parent1 == parent2) { // Save time as the offspring will be the same as the parents:
+                offsprings.add(parent1);
+                offsprings.add(parent2);
+                continue;
+            }
 
-			offsprings.add(offspring1);
-			offsprings.add(offspring2);
-		}
+            Chromosome offspring1 = new Chromosome(), offspring2 = new Chromosome();
 
-		// Torna os cromossomos viáveis caso não sejam
-		for (Chromosome offspring : offsprings) {
-			offspring = fixChromosome(offspring);
-		}
+            for (int j = 0; j < chromosomeSize; j++)
+                if (Math.random() < 0.5) {
+                    offspring1.add(parent1.get(j));
+                    offspring2.add(parent2.get(j));
+                } else {
+                    offspring1.add(parent2.get(j));
+                    offspring2.add(parent1.get(j));
+                }
 
-		return offsprings;
-	}
+            // Make the invalid chromosomes viable:
+            fixChromosome(offspring1);
+            fixChromosome(offspring2);
+
+            offspring1.fitness = fitness(offspring1);
+            offspring2.fitness = fitness(offspring2);
+            offsprings.add(offspring1);
+            offsprings.add(offspring2);
+        }
+
+        return offsprings;
+    }
 }
